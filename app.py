4acec1592132ba11908bf31be09ecbaa5e29c81d
Isaac34mi <itettehbvu@gmail.com>
2017-01-30 11:07:48 -0600

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ app = Flask(__name__)
 
 site = defaultdict(lambda: "404.html",{"resume": "resume.html", "about":"about.html", "project1":"projects.html"})
 
+@app.route('/')
+def index():
+	return render_template("resume.html")
 
 @app.route('/<page_name>')
 def pages(page_name):
